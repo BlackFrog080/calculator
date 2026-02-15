16849f0aa8e6708d8ad3f7197032a519c3eb7f8e
Maciej <stefanskimaciej33@gmail.com>
2026-02-15 19:41:31 +0100

    Dodano mnożenie i dzielenie

--- calculator.py
+++ calculator.py
@@ -4,6 +4,14 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
+    print("2 * 3 =", multiply(2, 3))
+    print("5 / 2 =", divide(5, 2))
