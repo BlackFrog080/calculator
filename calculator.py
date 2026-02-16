ca3164dca0aeaf67f62cacbb17765984e977ff9c
Maciej <stefanskimaciej33@gmail.com>
2026-02-16 13:17:17 +0100

    Dodaj pierwiastek kwadratowy

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
     return a + b
 
@@ -13,6 +15,9 @@ def divide(a, b):
 def power(a, b):
     return a ** b
 
+def sqrt(a):
+    return math.sqrt(a)
+
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
@@ -20,3 +25,5 @@ if __name__ == "__main__":
     print("5 / 2 =", divide(5, 2))
     print("2 ^ 3 =", power(2, 3))
     print("5 ^ 0 =", power(5, 0))
+    print("/2 =", sqrt(2))
+    print("/5 =", sqrt(5))
