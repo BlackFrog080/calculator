794b4e1f2d356eea0e84747876e122574db27b2a
Maciej <stefanskimaciej33@gmail.com>
2026-02-16 13:11:19 +0100

    Dodaj funkcję power

--- calculator.py
+++ calculator.py
@@ -10,8 +10,13 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+def power(a, b):
+    return a ** b
+
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
     print("5 - 2 =", subtract(5, 2))
     print("2 * 3 =", multiply(2, 3))
     print("5 / 2 =", divide(5, 2))
+    print("2 ^ 3 =", power(2, 3))
+    print("5 ^ 0 =", power(5, 0))
